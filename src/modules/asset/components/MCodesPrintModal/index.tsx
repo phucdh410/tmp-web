b3e226ffe2f6c68cbb8a64d578d4348df71f01bb
phucdh410 <phucdh410@gmail.com>
2024-09-19 15:22:36 +0700

    UPDATE: disable change to qrcode

--- src/modules/asset/components/MCodesPrintModal/index.tsx
+++ src/modules/asset/components/MCodesPrintModal/index.tsx
@@ -69,6 +69,7 @@ export const MCodesPrintModal = forwardRef<
               ]}
               value={codeType}
               onChange={setCodeType}
+              disabled
             />
           </Stack>
         </Stack>
