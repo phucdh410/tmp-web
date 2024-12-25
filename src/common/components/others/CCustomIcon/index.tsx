c8a7b7df9c18f4e4723e16497d1ab49033de014e
phucdh410 <phucdh410@gmail.com>
2024-12-25 15:05:43 +0700

    UPDATE: icons

--- src/common/components/others/CCustomIcon/index.tsx
+++ src/common/components/others/CCustomIcon/index.tsx
@@ -1,5 +1,8 @@
+import CheckedIcon from "@assets/icons/checked.svg?react";
 import ExportExcelIcon from "@assets/icons/excel-1.svg?react";
 import ImportExcelIcon from "@assets/icons/excel-2.svg?react";
+import IndeterminateIcon from "@assets/icons/indeterminate.svg?react";
+import UncheckIcon from "@assets/icons/uncheck.svg?react";
 import { SvgIcon, SvgIconProps } from "@mui/material";
 
 export const CImportExcelIcon = ({
@@ -23,6 +26,45 @@ export const CExportExcelIcon = ({
     <SvgIcon
       {...props}
       component={ExportExcelIcon}
+      inheritViewBox={inheritViewBox}
+    />
+  );
+};
+
+export const CUncheckIcon = ({
+  inheritViewBox = true,
+  ...props
+}: SvgIconProps) => {
+  return (
+    <SvgIcon
+      {...props}
+      component={UncheckIcon}
+      inheritViewBox={inheritViewBox}
+    />
+  );
+};
+
+export const CCheckedIcon = ({
+  inheritViewBox = true,
+  ...props
+}: SvgIconProps) => {
+  return (
+    <SvgIcon
+      {...props}
+      component={CheckedIcon}
+      inheritViewBox={inheritViewBox}
+    />
+  );
+};
+
+export const CIndeterminateIcon = ({
+  inheritViewBox = true,
+  ...props
+}: SvgIconProps) => {
+  return (
+    <SvgIcon
+      {...props}
+      component={IndeterminateIcon}
       inheritViewBox={inheritViewBox}
     />
   );
